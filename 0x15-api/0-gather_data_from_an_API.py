9691d04b0682155d0ab8c16d5ca95bc051c08afb
Yirga Mulaw <infoyirga12@gmail.com>
2023-04-26 19:31:41 +0300

    0. Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# Python script that returns information about TODO list progress.
+# Requirements:
+#
+#   You must use urllib or requests module
+#   The script must accept an integer as a parameter, which is the employee ID
+#   The script must display on the standard output the employee TODO list
+#       progress in this exact format:
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
+
+    response = requests.get(url)
+    employeeName = response.json().get('name')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
+
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
+
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
