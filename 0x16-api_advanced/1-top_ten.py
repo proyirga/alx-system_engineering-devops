d2e3f627b0aeb8f3663f1cf5199ad597b28d93e7
Yirga Mulaw <infoyirga12@gmail.com>
2023-08-07 14:21:13 +0300

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from requests import get
+
+
+def top_ten(subreddit):
+    """
+    prints the titles of the first 10 hot posts
+    """
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Linux arm; rv:99.0"}
+    params = {'limit': '10'}
+    response = get(url, headers=headers, params=params)
+    if response.status_code == 404:
+        print("None")
+        return
+    children = response.json().get('data').get('children')
+
+    for child in children:
+        print(child.get('data').get('title'))
