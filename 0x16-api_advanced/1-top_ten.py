5566bb957c9cfa47a1da1a4064f46b0e56f55779
Yirga Mulaw <infoyirga12@gmail.com>
2023-08-09 09:04:11 +0300

    Documentation

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,14 +2,7 @@
 """
 Write a function that queries the Reddit API and prints the 
 titles of the first 10 hot posts listed for a given subreddit.
-
-Requirements:
-
-    Prototype: def top_ten(subreddit)
-    If not a valid subreddit, print None.
 """
-
-
 from requests import get
 
 
