812129af4c3e26f08132985011821f79a82cde2e
Yirga Mulaw <infoyirga12@gmail.com>
2023-08-18 10:17:20 +0300

    Allow user holberton to login

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# Allow user holberton to login without problem
+exec { 'hard-file-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'soft-file-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
